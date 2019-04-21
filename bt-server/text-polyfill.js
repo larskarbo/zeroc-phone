12c143c8c281960fabedcb2ab92da84fb8bbac42
Lars Karbo <mail@larskarbo.no>
2019-04-21 13:53:44 +0200

    delete unused folder bt-server

--- bt-server/text-polyfill.js
+++
@@ -1,5 +0,0 @@
-if (typeof TextEncoder === 'undefined' && typeof TextDecoder === 'undefined') {
-    const encoding = require('text-encoding'); // polyfill
-    global.TextEncoder = encoding.TextEncoder;
-    global.TextDecoder = encoding.TextDecoder;
-}
